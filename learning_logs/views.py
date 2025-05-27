6f61c4d4441b23c6f29149b59c3778e241969707
Tony Kwong <tonykkn4321@gmail.com>
2025-05-27 10:50:35 +0800

    3rd commit:adding views

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
+def index(request):
+    """學習筆記的主頁。"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """顯示所有的主題。"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """顯示單個主題及其所有的條目。"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
