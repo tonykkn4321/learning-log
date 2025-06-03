4d66d0c484e837e94161df43a137135fa149896e
Tony Kwong <tonykkn4321@gmail.com>
2025-06-03 09:34:53 +0800

    6th commit:added register page for normal users

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 """為應用程序users定義URL模式。"""
 
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     # 包含默認的身份驗證URL。
     path('', include('django.contrib.auth.urls')),
+    # 注冊頁面
+    path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    """注冊新用戶。"""
+    if request.method != 'POST':
+        # 顯示空的注冊表單。
+        form = UserCreationForm()
+    else:
+        # 處理填寫好的表單。
+        form = UserCreationForm(data=request.POST)
+    
+        if form.is_valid():
+            new_user = form.save()
+            # 讓用戶自動登錄，再重定向到主頁。
+        login(request, new_user)
+        return redirect('learning_logs:index')
+
+    # 顯示空表單或指出表單無效。
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
