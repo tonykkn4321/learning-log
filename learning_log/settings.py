f92cf0f9874d5cfc42ef2bfb4d525cb179c67e49
Tony Kwong <tonykkn4321@gmail.com>
2025-06-22 09:47:17 +0800

    18th commit: Missing some imports in setting.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+import sys
+import json
+import base64
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
